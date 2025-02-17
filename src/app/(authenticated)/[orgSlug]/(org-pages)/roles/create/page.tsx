6d6312f1fdc8ec2094c150e07edbe59024fdf4b7
Jan Manaloto <janmanaloto@gmail.com>
2025-02-17 03:18:00 -0500

    create role permissions

--- src/app/(authenticated)/[orgSlug]/(org-pages)/roles/create/page.tsx
+++ src/app/(authenticated)/[orgSlug]/(org-pages)/roles/create/page.tsx
@@ -1,9 +1,15 @@
 import { OrgAccessHOCProps, withOrgAccess } from "@/lib/hoc/org";
 import GroupRoleForm from "@/app/(authenticated)/[orgSlug]/(org-pages)/roles/create/_components/group-role-form";
+import { permissions } from "@/lib/types/permissions";
 
 async function RolePage({ org, params }: OrgAccessHOCProps) {
   const _params = await params;
   return <GroupRoleForm groupId={org.id} redirectTo={`/@${org.slug}/roles`} />;
 }
 
-export default withOrgAccess(RolePage);
+export default withOrgAccess(RolePage, {
+  permissions: [permissions.roles.create],
+  onAccessDenied: {
+    action: "error"
+  },
+});
